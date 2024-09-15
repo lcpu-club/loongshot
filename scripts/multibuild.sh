2ca82ce8a3508a55750321a307ab165a00808ca5
Pluto Yang <yangyj.ee@gmail.com>
2024-09-15 15:06:09 +0800

    feat: multibuild: more info digged from logs

--- scripts/multibuild.sh
+++ scripts/multibuild.sh
@@ -2,6 +2,8 @@
 
 # "--sign" to sign the package
 SIGN=
+ALLLOGS=all.log
+
 max_retries=2
 total_pkgs=$(wc -l < pkg)
 current_pkg=0
@@ -11,12 +13,12 @@ for i in $(cat pkg); do
     ((current_pkg++))
 
     while [[ $retries -lt $max_retries ]]; do
-        ./buildpkg.sh "$i" $SIGN --stag --keepsrc -- --skipinteg | tee all.log
+        ./buildpkg.sh "$i" $SIGN --stag --keepsrc -- --skipinteg | tee $ALLLOGS
         exit_code=${PIPESTATUS[0]}
         ((retries++))
 
         # restart to download the corrupted packages
-        grep "pkg.tar.zst is corrupted" all.log >/dev/null 2>&1 && continue
+        grep "pkg.tar.zst is corrupted" $ALLLOGS >/dev/null 2>&1 && continue
 
         if [[ ! $exit_code -eq 5 ]]; then
             break
@@ -24,10 +26,19 @@ for i in $(cat pkg); do
     done
 
     # tracking the soname changes
-    grep -A4 "WARNING.*Sonames differ" all.log >> sonames
-    grep -q "unable to guess system type" all.log && echo $i >> configs
+    grep -A4 "WARNING.*Sonames differ" $ALLLOGS >> sonames
+
+    # tracking packages needs to update config.{guess, sub}
+    grep -q "unable to guess system type" $ALLLOGS && echo $i >> configs
+
+    # exit_code might be 5 in this case
+    grep -q "error: unresolvable package conflicts detected" $ALLLOGS && (echo $i >> conflicts; exit_code=6)
+
+    # download link breaks
+    grep -q "The requested URL returned error: 404" $ALLLOGS && echo $i >> 404s
+
     mkdir -p ~/workdir/build/$i/
-    mv all.log ~/workdir/build/$i/
+    mv $ALLLOGS ~/workdir/build/$i/
 
     if [[ $exit_code -eq 5 ]]; then
         echo "$(date '+%Y-%m-%d %H:%M:%S') - $i" >> netfail
