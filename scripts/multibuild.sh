be0c2ab02e83701fd3ce442f9fb2a96a445883c8
Pluto Yang <yangyj.ee@gmail.com>
2024-10-26 21:39:41 +0800

    feat: multibuild: use loong-build now

--- scripts/multibuild.sh
+++ scripts/multibuild.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# "--sign" to sign the package
-SIGN=
-ALLLOGS=all.log
-
-max_retries=3
+max_retries=2
 total_pkgs=$(wc -l < pkg)
 current_pkg=0
+
 umask 022
 rm -f multistop
 for i in $(cat pkg); do
@@ -24,53 +21,21 @@ for i in $(cat pkg); do
     else
         NOCHECK=""
     fi
+
     while [[ $retries -lt $max_retries ]]; do
-        STARTTIME=$SECONDS
-        ./buildpkg.sh "$i" $SIGN $NOCHECK "$@" | tee $ALLLOGS
-        exit_code=${PIPESTATUS[0]}
+        ./loong-build.sh "$i" --test $NOCHECK "$@"
         ((retries++))
+        PKGVER=${source $WORKDIR/$i/PKGBUILD; echo $epoch${epoch:+:}$pkgver-$pkgrel)
+        ALLLOGS=$WORKDIR/$i/$i-$PKGVER.log
 
         # restart to download the corrupted packages
         grep "pkg.tar.zst is corrupted" $ALLLOGS >/dev/null 2>&1 && continue
+        # update config.{sub,guess} and retry
         grep -q "unable to guess system type" $ALLLOGS && echo $i >> ~/loongarch-packages/update_config && continue
-        grep -q "error: target not found:" $ALLLOGS
-        if [[ $? -eq 0 ]]; then
-            exit_code=6
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - $i" >> missing.log
-            break
-        fi
-
-        if [[ ! $exit_code -eq 5 ]]; then
-            break
-        fi
     done
-    ENDTIME=$SECONDS
-    TIMECOST=$((ENDTIME - STARTTIME))
-    mkdir -p ~/workdir/build/$i/
-    echo $TIMECOST > ~/workdir/build/$i/.timecost
 
     # tracking the soname changes
     grep -A4 "WARNING.*Sonames differ" $ALLLOGS >> sonames.log
-
-    # exit_code might be 5 in this case
-    grep -q "error: unresolvable package conflicts detected" $ALLLOGS && (echo $i >> conflicts.log; exit_code=7)
-
-    # download link breaks
-    grep -q "The requested URL returned error: 404" $ALLLOGS && echo p"$(date '+%Y-%m-%d %H:%M:%S') - $i" >> 404s.log
-
-    mv $ALLLOGS ~/workdir/build/$i/
-
-    if [[ $exit_code -eq 5 ]]; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - $i" >> netfail.log
-    fi
-
-    if [[ $exit_code -eq 3 ]]; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - $i" >> patchfail.log
-    fi
-
-    if [[ $exit_code -eq 4 ]]; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - $i" >> buildfail.log
-    fi
 
     echo "$i - $current_pkg/$total_pkgs" > progress.log
 
