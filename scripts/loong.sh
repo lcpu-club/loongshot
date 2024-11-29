74a2d3cd5cfc00bec84e92b813ae5a539db633e9
Pluto Yang <yangyj.ee@gmail.com>
2024-11-29 13:48:27 +0800

    chore: remove unused loong.sh

--- scripts/loong.sh
+++
@@ -1,7 +0,0 @@
-LOONGREPO=/home/arch/loongarch-packages
-BUILDER=loong
-WORKDIR=/home/arch/workdir/
-WEBSRV=http://127.0.0.1
-REPOS=/srv/http/loongarch/archlinux
-PACKAGER="Jhon Doe <john@doe.com>"
-T0SERVER=arch2
