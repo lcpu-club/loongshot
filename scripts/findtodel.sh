906cff8a68e36cafdd603bfe5ef69cf3f68e2cfc
Pluto Yang <yangyj.ee@gmail.com>
2025-06-21 19:59:39 +0800

    fix: findtodel: remove deprecated packages

--- scripts/findtodel.sh
+++ scripts/findtodel.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-
-PAGER=cat psql -U pguser -d archdb -c "select name from packages where x86_version is Null" | sed 's/^ //;s/\r$//;1,2d;$d' | sed '$d' > /tmp/missing.lst
-
+PAGER=cat psql -U pguser -d archdb -c "select name from packages where x86_version is Null and x86_testing_version is Null and x86_staging_version is Null" | sed 's/^ //;s/\r$//;1,2d;$d' | sed '$d' > /tmp/missing.lst
 # don't remove the pacakges uniqe to loong
 sed -i '/loong/d;/x86_64-linux/d;/lcpu/d;/yt6801/d' /tmp/missing.lst
 
 BACKUP=$(mktemp -d)
+REPODIR=/srv/http/loongarch/archlinux
 echo "Backup dir is $BACKUP"
 
-cd /srv/http/loongarch/archlinux/extra/os/loong64
+cd $REPODIR/extra/os/loong64
 
-for i in `cat /tmp/missing.lst`; do
-    FILE=$(repo-remove extra.db.tar.gz $i | grep Removing | sed -n "s/.*'\(.*\)'.*/\1/p")
-    if [[ ! -z "$FILE" ]]; then
-        mv $FILE*.zst $BACKUP
-        mv $FILE*.sig $BACKUP
-    fi
+repo-remove extra.db.tar.gz `cat /tmp/missing.lst` | tee remove.log
+about_to_delete=()
+for pkg in $(grep -oP "Removing existing entry '\K[^']*(?=')" remove.log); do
+    about_to_delete+=($pkg)
 done
+if [ ! "${#about_to_delete[@]}" -eq 0 ]; then
+    for file in "${about_to_delete[@]}"; do
+        rm $file*.zst
+        rm $file*.sig
+        mv $REPODIR/pool/packages/$file*.zst $BACKUP
+        mv $REPODIR/pool/packages/$file*.sig $BACKUP
+    done
+fi
+rm remove.log
