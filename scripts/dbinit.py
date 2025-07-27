f87f0999c5a9f590d9467c5f1e4d9b064b64b519
NakanoMiku <jinleho@hotmail.com>
2025-07-27 20:04:48 +0000

    refactor: Use postgresql instead of sqlite3
    To increase compatability with website backend

--- scripts/dbinit.py
+++ scripts/dbinit.py
@@ -1,11 +1,31 @@
 import argparse
-import sqlite3
+import json
+import psycopg2
 
-compare_methods=['core_pkg', 'extra_pkg', 'all_pkg', 'build_pkg', 'packages']
+compare_methods=['core_pkg', 'extra_pkg', 'all_pkg', 'build_pkg']
+
+def load_config(config_file):
+    try:
+        with open(config_file, 'r') as f:
+            return json.load(f)
+    except Exception as e:
+        print(f"Error loading configuration file: {e}")
+        raise
+
+def get_conn(config):
+    config = load_config(config)
+
+    conn = psycopg2.connect(
+        dbname=config['database']['name'],
+        user=config['database']['user'],
+        password=config['database']['password'],
+        host=config['database']['host']
+    )
+    return conn
 
 # Write packages to database
 def create_database(db):
-    conn = sqlite3.connect(db)
+    conn = get_conn(db)
     cursor = conn.cursor()
     # cursor.execute(f'''DROP TABLE IF EXISTS {compare_method} ''')
     # Create a table for each compare method
@@ -23,6 +43,21 @@ def create_database(db):
         loong_staging_version TEXT
         )
         ''')
+
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS packages (
+        name TEXT PRIMARY KEY,
+        base TEXT,
+        repo TEXT,
+        flags INTEGER,
+        x86_version TEXT,
+        x86_testing_version TEXT,
+        x86_staging_version TEXT,
+        loong_version TEXT,
+        loong_testing_version TEXT,
+        loong_staging_version TEXT
+        )
+    ''')
 
     conn.commit()
     cursor.close()
