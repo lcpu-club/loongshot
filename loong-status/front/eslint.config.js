07bd7ce8da642c7387d7495f289d160ea7625e3d
Pluto Yang <yangyj.ee@gmail.com>
2025-10-31 17:32:29 +0800

    chore: forget to add eslint.config.js

---
+++ loong-status/front/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettier.rules,
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+  },
+];
